db0ef2176a76e9e636106d6fe5a89d814cc2bdc6
Neiman22 <pushkarev_av@inbox.ru>
2024-04-09 16:01:07 +0300

    fix: add window.Telegram.WebApp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { useEffect } from 'react';
 import './App.css';
+const tg = window.Telegram.WebApp;
 
 function App() {
+
+  useEffect(() => {
+    tg.ready();
+  }, [])
+
+  const onClose = () => {
+    tg.close();
+  }
+
   return (
     <div className="App">
+      Hello React
+      <button onClick={onClose}>Закрыть</button>
     </div>
   );
 }
